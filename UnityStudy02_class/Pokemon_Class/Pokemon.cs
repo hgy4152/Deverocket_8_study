bdff456fd1ddccb0b68202c9b6c78515e261a86b
Giyeong - Heo <hgy4152@gmail.com>
2026-02-26 12:11:53 +0900

    UnityStudy02_class
    포켓몬 도감 등록 class 사용 버전

---
+++ UnityStudy02_class/Pokemon_Class/Pokemon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class Pokemon
+{
+    private string name;
+    private int attack;
+    private int defense;
+    private string type;
+    public static int totalCount = 0;
+
+    public readonly int health;
+
+    public Pokemon(string name, int attack, int defense, string type, int health)
+    {
+        this.name = name;
+        this.attack = attack;
+        this.defense = defense;
+        this.type = type;
+        this.health = health;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"[{name}] 타입: {type}");
+        Console.WriteLine($"HP: {health}");
+        Console.WriteLine($"Atk: {attack}");
+        Console.WriteLine($"Def: {defense}");
+        Console.WriteLine();
+    }
+}
---
+++ UnityStudy02_class/Pokemon_Class/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Pokemon poke1 = new Pokemon("피카츄", 55, 40, "전기", 35);
+Pokemon.totalCount++;
+poke1.ShowInfo();
+
+Pokemon poke2 = new Pokemon("파이리", 52, 43, "불꽃", 39);
+Pokemon.totalCount++;
+poke2.ShowInfo();
+
+Pokemon poke3 = new Pokemon("꼬부기", 48, 65, "물", 44);
+Pokemon.totalCount++;
+poke3.ShowInfo();
+
+Pokemon poke4 = new Pokemon("이상해씨", 49, 49, "풀", 45);
+Pokemon.totalCount++;
+poke4.ShowInfo();
+
+Pokemon poke5 = new Pokemon("잠만보", 110, 65, "노말", 160);
+Pokemon.totalCount++;
+poke5.ShowInfo();
+
+
+Console.WriteLine($"총 포켓몬 수: {Pokemon.totalCount}");
