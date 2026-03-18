aae521272da0adf1ccb1be9329230a6a0458190e
Giyeong - Heo <hgy4152@gmail.com>
2026-03-18 16:49:39 +0900

    study

---
+++ UnityStudy03_Inheritance/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿var action1 = Capture();
+var action2 = Capture();
+
+action1();
+action1();
+action2();
+action2();
+
+static Action Capture()
+{
+    int count = 0;
+    string name = "수명연장";
+
+    return () => Console.WriteLine(name[count++]);
+}
---
+++ UnityStudy03_Inheritance/ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+string[] names = { "Tom", "Dick", "Harry", "Mary", "Jay" };
+
+// 'a' 포함 → 길이순 정렬 → 대문자 변환
+var query = names
+    .Where(n => n.Contains("a"))
+    .OrderBy(n => n.Length)
+    .Select(n => n.ToUpper());
+
+
+foreach (var name in query)
+{
+    Console.WriteLine(name);
+}
